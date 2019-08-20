3423389bf1f1890f475093e65065bd8e36d9c635
Momchil <leatherpuffy@gmail.com>
2019-08-20 17:36:29 +0300

    Created ItemController and BaseController for cleaner authorization

---
+++ Auction/Web/Auction.Web/Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.Web.Controllers
+{
+    public class ItemController : BaseController
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
