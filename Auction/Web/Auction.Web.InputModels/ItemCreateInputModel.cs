36f5be0abe41a89ed16927db73e80dfd80edad9b
Momchil <leatherpuffy@gmail.com>
2019-08-20 17:39:00 +0300

    Created Item create input model

---
+++ Auction/Web/Auction.Web.InputModels/ItemCreateInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Auction.Web.InputModels
+{
+    public class ItemCreateInputModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal StartingPrice { get; set; }
+
+        public decimal BuyOutPrice { get; set; }
+
+        public int AuctionDuration { get; set; }
+
+        public string AuctionHouse { get; set; }
+    }
+}
