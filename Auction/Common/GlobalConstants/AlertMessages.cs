912a62193e30bdb4e9ea0528d044bbb18ed1da36
Momchil <leatherpuffy@gmail.com>
2019-08-26 20:57:15 +0300

    Clean up and add AlertMessages

---
+++ Auction/Common/GlobalConstants/AlertMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalConstants
+{
+    public static class AlertMessages
+    {
+        public const string CitySuccessfullyCreated = "City has been successfully created!";
+
+        public const string ItemNotFound = "Oops! Looks like the item you're searching for actually does not exist.";
+    }
+}
--- Auction/Common/GlobalConstants/BasicConstants.cs
+++ Auction/Common/GlobalConstants/BasicConstants.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GlobalConstants
+﻿namespace GlobalConstants
 {
     public static class BasicConstants
     {
         public const string Admin = "Admin";
-
         public const string AdminNormalized = "ADMIN";
 
         public const string AdministrationArea = "Administration";
 
         public const string User = "User";
-
         public const string UserNormalized = "USER";
+
+        public const string TempErrorMessage = "ErrorMessage";
+        public const string TempSuccessMessage = "SuccessMessage";
+
+        public const string ProfileRoute = "ProfileDetails";
     }
 }
